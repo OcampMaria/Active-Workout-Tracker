2007a8d7dfa14c3966f343e5289764b40f062cd5
Ocampo <mariaocampo0513@gmail.com>
2021-02-22 09:15:06 -0800

    modified server.js. changed port and imported Morgan

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan")
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(morgan("dev"));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTracker", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
